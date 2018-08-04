bc8633ba7807576306e4352f46773583d69eab5e
DmytroHaidash <dmytro.haidash@gmail.com>
2018-08-04 22:24:10 +0300

    second commit

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 //created by Dmytro_Haidash
-$a = 'its my first progect';
-var_dump($a);
+header("Content-Type: text/plain; charset=utf-8");
+echo "www - world wide web - всемирная сеть, сеть сетей. ";
+echo "\n";
+echo 'Домен - это адрес в сети.';
+echo "\n";
+echo 'Сервер - это компьютер. ';
+echo "\n";
+echo 'Хостинг = это место на компьютере с которого скачиваются файлы.';
 ?>
